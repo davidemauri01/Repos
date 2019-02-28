7467ac5e4eb41387b73193fb6917ca150a3692f7
Mauri_Davide <Mauri_Davide@INF1-15.jeanmonnet.local>
2019-02-28 10:54:52 +0100

    Aggiunta Semafori per ordinare i suoni

--- DinDonDan/src/Main.java
+++ DinDonDan/src/Main.java
@@ -1,5 +1,6 @@
 
 import java.util.Scanner;
+import java.util.concurrent.Semaphore;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -18,10 +19,13 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Semaphore sDin = new Semaphore(1);
+        Semaphore sDon = new Semaphore(0);
+        Semaphore sDan = new Semaphore(0);
         DatiCondivisi d = new DatiCondivisi();
-        MyThread Din = new MyThread(d, 0);
-        MyThread Don = new MyThread(d, 1);
-        MyThread Dan = new MyThread(d, 2);
+        MyThread Din = new MyThread(d, 0, sDin, sDon);
+        MyThread Don = new MyThread(d, 1, sDon, sDan);
+        MyThread Dan = new MyThread(d, 2, sDan, sDin);
         Scanner s = new Scanner(System.in);
         
         
--- DinDonDan/src/MyThread.java
+++ DinDonDan/src/MyThread.java
@@ -1,3 +1,6 @@
+
+import java.util.concurrent.Semaphore;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,16 +14,26 @@
 public class MyThread extends Thread{
     private DatiCondivisi d;
     private Integer numThread;
+    private Semaphore s;
+    private Semaphore sThSuccessivo;
     
-    public MyThread(DatiCondivisi d, int numThread){
+    public MyThread(DatiCondivisi d, int numThread,Semaphore s, Semaphore sThSuccessivo){
         this.d = d;
         this.numThread = numThread;
+        this.s = s;
+        this.sThSuccessivo = sThSuccessivo;
     }
     
     @Override
-    public void run(){
-            while(!isInterrupted()){
+    public void run() {
+        try {
+            while (!isInterrupted()) {
+                s.acquire();
                 d.stampaSuono(numThread);
-            }        
+                sThSuccessivo.release();
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
     }
 }
